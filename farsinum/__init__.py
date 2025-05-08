b8c4bfe1972d8604eb8b679e8e0c2582a1802f11
sajjadeakbari <sajjadakbari.dv@gmail.com>
2025-05-08 19:41:45 +0330

    Update __init__.py

--- farsinum/__init__.py
+++ farsinum/__init__.py
@@ -1,24 +1,29 @@
 # farsinum/__init__.py
 
 """
-Farsinum: یک کتابخانه پایتون برای کار با اعداد، ارقام و متن فارسی.
-شامل توابعی برای تبدیل اعداد، تبدیل عدد به حروف، نرمال‌سازی متن فارسی و تحلیل ساده متن.
+Farsinum: یک کتابخانه پایتون برای کار با اعداد، ارقام، متن و تاریخ فارسی.
+شامل توابعی برای تبدیل اعداد، تبدیل عدد به حروف، نرمال‌سازی متن فارسی،
+تحلیل ساده متن و تبدیل تاریخ میلادی/شمسی.
 """
 
-__version__ = "0.2.0" # افزایش نسخه
+__version__ = "0.3.0" # افزایش نسخه
 
 from .numeral_converter import to_persian_numerals, to_english_numerals
 from .number_to_words import number_to_persian_words
 from .text_normalizer import (
     persian_text_normalizer,
-    normalize_characters,
-    cleanup_spacing,
-    standardize_quotes,
-    standardize_ellipsis,
-    add_zwnj_to_common_suffixes,
+    # ... (بقیه import های text_normalizer)
     ZWNJ
 )
 from .text_analyzer import count_words, count_sentences, count_paragraphs
+from .date_converter import ( # اضافه کردن ماژول جدید
+    gregorian_to_jalali,
+    jalali_to_gregorian,
+    today_jalali,
+    DEFAULT_GREGORIAN_FORMAT,
+    DEFAULT_JALALI_FORMAT,
+    COMMON_JALALI_FORMAT_WITH_DAY_NAME
+)
 
 
 # توابعی که می‌خواهیم با from farsinum import * در دسترس باشند
@@ -30,7 +35,7 @@ __all__ = [
     "number_to_persian_words",
     # Text Normalizer
     "persian_text_normalizer",
-    "normalize_characters",
+    "normalize_characters", # اگر می‌خواهید اینها هم مستقیما قابل import باشند
     "cleanup_spacing",
     "standardize_quotes",
     "standardize_ellipsis",
@@ -40,6 +45,13 @@ __all__ = [
     "count_words",
     "count_sentences",
     "count_paragraphs",
+    # Date Converter
+    "gregorian_to_jalali",
+    "jalali_to_gregorian",
+    "today_jalali",
+    "DEFAULT_GREGORIAN_FORMAT",
+    "DEFAULT_JALALI_FORMAT",
+    "COMMON_JALALI_FORMAT_WITH_DAY_NAME",
     # Version
     "__version__"
 ]
